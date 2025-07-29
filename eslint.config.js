89a4ca8799ae8aa35b60eb5ec684153119a8d332
pablod646 <30422763+pablod646@users.noreply.github.com>
2025-07-29 10:18:27 -0300

    fix: Re-commit eslint.config.js after previous attempt

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,32 @@
 import js from "@eslint/js";
 import svelte from "eslint-plugin-svelte";
-import jsxA11y from "eslint-plugin-jsx-a11y";
 import svelteParser from "svelte-eslint-parser";
 import typescriptParser from "@typescript-eslint/parser";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 export default [
   js.configs.recommended,
-  ...svelte.configs['flat/recommended'],
+  ...svelte.configs["flat/recommended"],
+  jsxA11y.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  },
   {
     files: ["**/*.svelte"],
     languageOptions: {
       parser: svelteParser,
       parserOptions: {
         parser: typescriptParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
-    },
-    plugins: {
-      svelte,
-      "jsx-a11y": jsxA11y,
-    },
-    rules: {
-      ...svelte.configs.recommended.rules,
-      ...jsxA11y.configs.recommended.rules,
     },
   },
 ];
